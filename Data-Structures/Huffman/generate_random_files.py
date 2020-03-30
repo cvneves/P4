46452083c3435f70dbe1d8b57cbbcda999883993
carlos <carloskic1@gmail.com>
2020-03-30 13:47:49 -0300

    a

--- Data-Structures/Huffman/generate_random_files.py
+++ Data-Structures/Huffman/generate_random_files.py
@@ -1,39 +1,14 @@
 import math
 import random
 import argparse
+import matplotlib.pyplot as plt
+import numpy as np
 
-min_file_size = 5000
-max_file_size = 5000000
-
-num_file_sizes = 2
-num_types = 1
-
-b = min_file_size
-a = (max_file_size - b)/(num_file_sizes - 1)
-
-folder_name = "random_instances/"
-
-for j in range(0, num_types):
-    instance_type = str(chr(98 + j))
-
-    for k in range(0, num_file_sizes):
-        file_name = instance_type + "-" + str(k)
-
-        fileBytes = []
-
-        fp = open(folder_name + file_name + ".bin", "wb")
-
-        sum = 0
-
-        fileSize = int(round(a * k + b))
-
-        for i in range(0,fileSize):
-            x = int(round(random.normalvariate(127, 64)))
-            # x = int(round(random.uniform(0, 256)))
-            x = x%256
-                
-            fileBytes.append(x)
+x = np.arange(0,1,0.01)
+a, b = 2,0.01
+y = np.random.normal(a,b,len(x))
+print(len(x))
 
 
-        fp.write(bytearray(fileBytes))
-    
+plt.plot(x, y, 'ro')
+plt.show()
