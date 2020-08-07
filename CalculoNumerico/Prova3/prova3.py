07ad5ed4b7413976ba940bfdefb75ee3d259a0a1
carlos <carloskic1@gmail.com>
2020-08-07 07:20:14 -0300

    a

--- CalculoNumerico/Prova3/prova3.py
+++ CalculoNumerico/Prova3/prova3.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 # INTEGRAIS
 
@@ -32,18 +33,33 @@ def trapezioComposto(f, a, b, n):
 def f3(x,y):
     return (2000 - 2*y)/(200 - x)
 
-def f4(x, y):
-    return y + 2 * x - x**2
+def f4(x,y):
+    return 2e-6*(100000 - y)*y
 
 def eulerAperfeicoado(f, h, x0, y0, x):
     xn = x0
     yn = y0
-    while xn < x:
+    while abs(xn - x) > 1e-8:
+        k1 = f(xn, yn)
+        k2 = f(xn + h, yn + h * k1)
+
         xn += h
-        k1 = h * f(xn, yn)
-        k2 = h * f(xn + h/2, yn + k1/2)
-        yn += h * f(xn, yn)
-        # yn += (k1 + k2)/2
+        yn += (k1 + k2) * h/2
+     
+    return yn
+
+def krutaOrdem4(f, h, x0, y0, x):
+    xn = x0
+    yn = y0
+    while abs(xn - x) > 1e-8:
+        k1 = np.float32(h * f(xn, yn))
+        k2 = np.float32(h * f(xn + h/2, yn + k1/2))
+        k3 = np.float32(h * f(xn + h/2, yn + k2/2))
+        k4 = np.float32(h * f(xn + h, yn + k3))
+        
+        yn += (k1 + 2*k2 + 2*k3 + k4)/6
+        xn += h
+        # print(xn,yn,k1,k2,k3,k4)
 
     return yn
 
@@ -54,5 +70,9 @@ print("Integral estimada: ", simpsonComposto(f1, -1, 1, 8))
 print("Simpson: ", simpsonComposto(f2, 0, math.pi, 6))
 print("Trapezio: ", trapezioComposto(f2, 0, math.pi, 6))
 
-# QUestao 3
-print("a: ", eulerAperfeicoado(f4, 0.1, 0,1,0.5))
+# Questao 3
+print(eulerAperfeicoado(f3, 0.5, 0, 0, 5))
+print(krutaOrdem4(f3, 0.5, 0,0,5))
+
+# Questao 4
+print(krutaOrdem4(f4, 0.5, 0,1000,5))
